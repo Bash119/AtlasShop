f78fe585c63bd6a263e0f6fc47fa42459223dbf3
BachSaab <BachSaab@ALYA-PF3F2WK3>
2024-12-27 18:45:11 -0500

    Basket-redis

--- atlas-shop/src/Services/Basket/BasketAPI/Program.cs
+++ atlas-shop/src/Services/Basket/BasketAPI/Program.cs
@@ -4,6 +4,10 @@
 
 
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -22,10 +26,26 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+
+});
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
+    });
 // Configure the HTTP request pipeline
 app.MapCarter();
 
