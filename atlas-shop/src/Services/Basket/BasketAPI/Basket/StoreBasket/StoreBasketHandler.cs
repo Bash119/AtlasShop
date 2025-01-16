c86ba077f8dad6770decaa1efd492e9246170d40
BachSaab <BachSaab@ALYA-PF3F2WK3>
2025-01-16 15:48:55 -0500

    Discount

--- atlas-shop/src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
+++ atlas-shop/src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Threading;
+using Discount.Grpc;
+
 namespace BasketAPI.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart):ICommand<StoreBasketResult>;
@@ -13,18 +16,36 @@ namespace BasketAPI.Basket.StoreBasket
             RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required!!");
         }
     }
-    public class StoreBasketCommandHandler(IBasketRepository repository)
+    public class StoreBasketCommandHandler
+        (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
+
+            await DeductDiscount(command.Cart,cancellationToken);
+
             
-            ShoppingCart cart = command.Cart;
             //TODO: store basket in database (use Marten upsert - if exist = update - if not= insert new record)
             //TODO: Update cache 
             await repository.StoreBasket(command.Cart,cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
         }
+
+        private async Task DeductDiscount(ShoppingCart cart,CancellationToken cancellationToken)
+        {
+            //TODO : Communicate with Discount.Grpc and calculate lastest prices of products into basket
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
+
+        }
     }
+
+ 
+
+
 }
--- atlas-shop/src/Services/Basket/BasketAPI/Program.cs
+++ atlas-shop/src/Services/Basket/BasketAPI/Program.cs
@@ -4,6 +4,7 @@
 
 
 
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Caching.Distributed;
@@ -11,6 +12,9 @@ using Microsoft.Extensions.Caching.Distributed;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+
+//Application Services
 var assembly=typeof(Program).Assembly;  
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
@@ -19,6 +23,8 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
@@ -27,7 +33,7 @@ builder.Services.AddMarten(opts =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
@@ -35,6 +41,24 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 });
 
+
+//Grpc Services
+//TODO - Add Grpc services 
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
+});
+
+//Cross-Cutting Services 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
